48de08cd0a307ee0bf5407225984135e28071319
Vinicius Calefo <vincalefo@gmail.com>
2024-07-04 15:49:15 -0300

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,4 +1,18 @@
 'use strict';
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 // Data needed for a later exercise
 const flights =
@@ -12,22 +26,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  //ES6 enchanced object literals
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+    //ES6 enchanced functios methods
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -47,28 +50,22 @@ const restaurant = {
   }
 };
 
-const rest1 = {
-  name: "Capri1",
-  numGuests: 10,
-}
+// OPTIONAL CHAINING
+if(restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open)
 
-const rest2 = {
-  name: "La Piazza",
-  owner: 'vKali',
-}
+//WITH OPTIONAL CHAINING
+console.log(restaurant.openingHours.mon?.open) 
 
-//OR assigment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 10 //More concise way
-// rest2.numGuests ||= 10 //More concise way
 
-//nuulish assigment operator(null or undefined)
-rest1.numGuests ??= 10 //More concise way
-rest2.numGuests ??= 10 //More concise way
 
-console.log(rest1)
-console.log(rest2)
+
+
+
+
+
+
+
+
 
 
 
@@ -267,3 +264,128 @@ console.log(rest2)
 // console.log('' && 'Vini'); //empy string is false
 // console.log(true && 0); // 0 is s false value
 // console.log(undefined && null); // undefined is a false value
+
+// const rest1 = {
+//   name: "Capri1",
+//   numGuests: 10,
+// }
+
+// const rest2 = {
+//   name: "La Piazza",
+//   owner: 'vKali',
+// }
+
+//OR assigment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10 //More concise way
+// rest2.numGuests ||= 10 //More concise way
+
+//nuulish assigment operator(null or undefined)
+// rest1.numGuests ??= 10 //More concise way
+// rest2.numGuests ??= 10 //More concise way
+
+// console.log(rest1)
+// console.log(rest2)
+
+
+
+
+
+// ///////////////////////////////////////
+// // Coding Challenge #1
+
+// /* 
+// We're building a football betting app (soccer for my American friends 😅)!
+
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+// GOOD LUCK 😀*/
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// //1. Create one player array for each team (variables 'players1' and 'players2')
+// const [players1,players2] = game.players
+// console.log(players1,players2)
+
+
+// // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// const [gk,...fieldPlayers] = players1
+// console.log(gk,fieldPlayers)
+
+// // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...players1,...players2]
+// console.log(allPlayers)
+
+// // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// const substitutions = ['Thiago', 'Coutinho' , 'Perisic']
+// const players1Final = [...players1,...substitutions]
+// console.log(players1Final)
+
+// // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const {team1,x:draw,team2} = game.odds 
+// console.log(team1,draw,team2,)
+
+// // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+// function printGoals(...players){
+//   console.log(`${players.length} goals were scored `)
+// }
+// printGoals(...game.scored)
+
+// // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// team1>team2 && console.log('Team 1 is more likely to win')
+// team2>team1 && console.log('Team 1 is more likely to win')
+
+
+
+// /*USING THE FOR OF LOOP*/
+
+// const menu = [...restaurant.starterMenu , ...restaurant.mainMenu]
+// for(const item of menu) console.log(item)
+
+// for(const item of menu.entries()) console.log(`${item[0]+1 }: ${item[1]}`) //if i want de index to old way
+// for(const [i,el] of menu.entries()) console.log(`${i+1 }: ${el}`) //if i want de index to in modern way
+
+
+
