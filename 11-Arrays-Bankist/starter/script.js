6241d6187a499f3098f958609ceced48f09e2491
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2024-07-22 14:25:45 -0300

    .
    .

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -279,6 +279,14 @@ console.log(x)
 arr.fill(23,4,6)
 console.log(arr)
 
+//Array.from
+const y = Array.from({length:7}, () => 1)
+console.log(y)
+
+const z = Array.from({lenght:7},(_, i) => i+1);
+console.log(z)
+
+
 
 
 //ARRAY METHODS
