64d64473d912e51eb0542d77410e2b82e2aafe22
Vinicius Calefo <vincalefo@gmail.com>
2024-07-30 15:47:54 -0300

    sticky navigation
    .

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -118,23 +118,66 @@ tabsContainer.addEventListener('click',function(e){
 
 
 //Menu fade Animations
-const handleHover = function(e){
-  if(e.target.classList.contains('nav__link')){
-    const link = e.target
-    const siblings = link.closest(".nav").querySelectorAll(".nav__link")
-    const logo = link.closest('.nav').querySelector('img')
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
 
-    siblings.forEach(el =>{
-      if(el !== link) el.style.opacity = this;
-    })
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
     logo.style.opacity = this;
-  
   }
+};
+//Passing a "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// //Sticky navigation
+// const initialCoords = section1.getBoundingClientRect()
+
+// window.addEventListener('scroll',function(){
+//   if(window.scrollY > initialCoords.top){
+//     nav.classList.add('sticky')
+//   }else {
+//     nav.classList.remove('sticky')
+//   }
+// })
+
+//STICKY NAVIGATION; INTERSECTIONS OBERVER API
+// const observerCallback = function(entries,observer){
+//   entries.forEach(entry=>{
+//     console.log(entry)
+//   })
+// }
+// const observerOptions = {
+//   root:null,
+//   threshold:[0 , 0.2],
+// }
+// const observer = new IntersectionObserver(observerCallback,observerOptions);
+// observer.observe(section1);
+const header = document.querySelector('.header')
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = function(entries) {
+  const [entry] = entries
+
+  if(!entry.isIntersecting) nav.classList.add('sticky')
+    else{
+  nav.classList.remove('sticky')}
 }
 
-//Passing a "argument" into handler
-nav.addEventListener('mouseover',handleHover.bind(0.5))
-nav.addEventListener('mouseout',handleHover.bind(1))
+const headerObserver = new IntersectionObserver(stickyNav,{
+  root:null,
+  threshold:0,
+  rootMargin:`-${navHeight}px`//header´s height
+});
+
+headerObserver.observe(header)
+
+
+
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
