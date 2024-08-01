cc69af8994447154a9ec9ae29cea6fad6ef0da59
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2024-07-31 21:23:44 -0300

    slider

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -193,38 +193,121 @@ const sectionObserver = new IntersectionObserver(revealSection,{
   threshold: 0.15,
 });
 
-allSections.forEach(function(section){
-  sectionObserver.observe(section)
-  section.classList.add('section--hidden')
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  // section.classList.add('section--hidden')
 });
 
 //LAZY LOADING IMAGE
-const imageTargets = document.querySelectorAll('img[data-src')
-const loadImg = function(entries,observer){
-  const [entry] = entries
+const imageTargets = document.querySelectorAll('img[data-src');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
 
-  if(!entry.isIntersecting)return;
+  if (!entry.isIntersecting) return;
 
   //Replace src with data-src
-  entry.target.src = entry.target.dataset.src
+  entry.target.src = entry.target.dataset.src;
 
-  entry.target.addEventListener('load',function(){
-    entry.target.classList.remove('lazy-img')
-  })
-  observer.unobserve(entry.target)
-}
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
 
-const imageObserver = new IntersectionObserver(loadImg,{
-  root:null,
-  threshold:0,
-  rootMargin:'-200px'
-})
+const imageObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
 
-imageTargets.forEach(img=>{
-  imageObserver.observe(img)
-})
+imageTargets.forEach(img => {
+  imageObserver.observe(img);
+});
 
+//SLIDER COMPONENT
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
 
+  //dots
+  const dotContainer = document.querySelector('.dots');
+  /////////////////////
+  //check slide
+  let currentSlide = 0;
+  const maxSlide = slides.length;
+
+  //Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('.dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - currentSlide)}%)`)
+    );
+  };
+
+  //Next Slide
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  };
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activeDot(0);
+  };
+  init();
+
+  //Event Handelers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', e => {
+    if (e.key == 'ArrowRight') nextSlide();
+    if (e.key == 'ArrowLeft') prevSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activeDot(currentSlide);
+    }
+  });
+};
+slider()
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
